5e6a96b376ffbe828ca805a33fcf55456c394d6d
lucdhaenen <luc.dhaenen@gmail.com>
2014-07-04 09:40:19 +0200

    Final
    Test Module LINQ

---
+++ LINQ_LandenStedenTalen_MySql/LINQ_LandenStedenTalen_MySql/Default.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQ_LandenStedenTalen_MySql
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!IsPostBack)
+                {
+                    using (var LSTEntities = new LandenStedenTalenEntities())
+                    {
+                        var LandenQuery = from land in LSTEntities.lst_landen
+                                          orderby land.Naam
+                                          select new { land.LandCode, land.Naam };
+                        landenListBox.DataValueField = "LandCode";
+                        landenListBox.DataTextField = "Naam";
+                        landenListBox.DataSource = LandenQuery.ToList();
+                        DataBind();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        protected void landenListBox_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var LSTEntities = new LandenStedenTalenEntities())
+                {
+                    string landCode;
+                    if (landenListBox.SelectedValue != null)
+                    {
+                        landCode = landenListBox.SelectedValue;
+                        var StedenQuery = from stad in LSTEntities.lst_steden
+                                          where stad.LandCode == landCode
+                                          orderby stad.Naam
+                                          select stad.Naam;
+                        stedenListBox.DataSource = StedenQuery.ToList();
+                        stedenListBox.DataBind();
+
+                        var TalenQuery = from t in LSTEntities.lst_talen
+                                         from l in t.lst_Landen
+                                         where l.LandCode == landCode
+                                         select t.Naam;
+                        talenListBox.DataSource = TalenQuery.ToList();
+                        talenListBox.DataBind();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        protected void ToevoegenButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var LSTEntities = new LandenStedenTalenEntities())
+                {
+                    if (landenListBox.SelectedIndex >= 0)
+                    {
+                        lst_Stad stad = new lst_Stad
+                        {
+                            Naam = stadTextBox.Text,
+                            LandCode = landenListBox.SelectedValue
+                        };
+                        if (stadTextBox.Text != "")
+                        {
+                            LSTEntities.lst_steden.Add(stad);
+                            LSTEntities.SaveChanges();
+                            Response.Write("De entity is toegevoegd");
+                            stadTextBox.Text = "";
+                        }                        
+                    }
+                    else
+                    {
+                        Response.Write("Gelieve een land te selecteren");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
---
+++ LINQ_LandenStedenTalen_MySql/LINQ_LandenStedenTalen_MySql/obj/Release/Package/PackageTmp/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="LINQ_LandenStedenTalen_MySql.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ListBox ID="landenListBox" runat="server" Height="130px" AutoPostBack="true" Width="125px" OnSelectedIndexChanged="landenListBox_SelectedIndexChanged1"></asp:ListBox>
+        <br />
+        <br />
+        <asp:ListBox ID="stedenListBox" runat="server" Height="139px" Width="255px"></asp:ListBox>
+        <br />
+        <br />
+        <asp:ListBox ID="talenListBox" runat="server" Height="395px" Width="262px"></asp:ListBox>
+        <br />
+        <br />
+        <asp:TextBox ID="stadTextBox" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator runat="server" ErrorMessage="Verplicht stad in te vullen" ControlToValidate="stadTextBox"></asp:RequiredFieldValidator>
+        <asp:Button ID="ToevoegenButton" runat="server" Text="Stad toevoegen" OnClick="ToevoegenButton_Click" />
+    </div>
+    </form>
+</body>
+</html>
